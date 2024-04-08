47905fe52e544a8b6cc8f270b05942b632e18d4f
aline coutinho <acoutinhoa@gmail.com>
2024-04-08 19:46:50 -0300

    djagoGirls/teplates

--- pf/views.py
+++ pf/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import *
 
 def index(request):
-    return render(request, 'pf/index.html', {})
+    lista = Projeto.objects.filter(visivel=True).order_by('-d0')
+    return render(request, 'pf/index.html', {'lista':lista})
